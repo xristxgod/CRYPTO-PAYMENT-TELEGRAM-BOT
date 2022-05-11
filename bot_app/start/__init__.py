b72d933cc67d0e880ea5781f81071078a980767e
Murad Mamedov <84931791+xristxgod@users.noreply.github.com>
2022-05-11 10:48:57 +0300

    Delete bot_app directory

--- bot_app/start/__init__.py
+++
@@ -1,2 +0,0 @@
-from bot_app.__init__ import dp, bot
-from src.models import UserModel
--- bot_app/start/states.py
+++
@@ -1,5 +0,0 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-class StateRegistrationForm(StatesGroup):
-    inviter_id = State()
-    lang_id = State()

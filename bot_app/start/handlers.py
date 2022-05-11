b44c4bfd3c58c2c4810894eccb6722ac916ada0c
Mamedov Murad <mamedov_99b@mail.ru>
2022-05-11 10:23:32 +0300

    [ADD] states.py

--- bot_app/start/handlers.py
+++ bot_app/start/handlers.py
@@ -1,8 +1,8 @@
-from aiogram.types import Message
+from aiogram import types
 
 from . import dp, bot
 
-@dp.message_handler(commands=["start"])
-async def start(message: Message):
+@dp.message_handler(commands=['start'], chat_type=types.ChatType.PRIVATE, state='*')
+async def start(message: types.Message, state: FS):
     if message.chat.type == "private":
         pass
